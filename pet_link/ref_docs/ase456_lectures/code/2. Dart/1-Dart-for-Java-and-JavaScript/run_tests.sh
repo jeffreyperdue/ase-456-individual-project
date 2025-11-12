fa687a66c1cf83514e8a736375a85fea0d813713
Jeffrey Perdue <perduej7@nku.edu>
2025-11-12 17:38:29 -0500

    updated documentation

--- pet_link/ref_docs/ase456_lectures/code/2. Dart/1-Dart-for-Java-and-JavaScript/run_tests.sh
+++
@@ -1,4 +0,0 @@
-# Run all tests
-dart test 
-# Run specific test file
-dart test/mixin_test.dart 
--- pet_link/ref_docs/ase456_lectures/code/2. Dart/1-Dart-for-Java-and-JavaScript/test/Mixins_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:test/test.dart';
-import '../lib/Mixins.dart'; // Adjust path as necessary
-
-void main() {
-  test('Fish can breathe underwater', () {
-    final fish = Fish();
-    expect(() => fish.breatheUnderwater(), prints("Breathing through gills!\n"));
-  });
-}  
